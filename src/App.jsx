601a3cf548c03d0a5336d3cedcfe0f2d80f154ac
Luan Carvalho <99412966+luancarvalho7@users.noreply.github.com>
2023-07-05 11:10:52 +0000

    guaranteee

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,24 @@
-import './App.css'
-import { Hero } from './components/hero/hero'
-import { Cta } from './components/cta/cta'
-import { Proof } from './components/proof/proof'
-import { Benefits } from './components/Benefits/benefits'
-import { Method } from './components/method/method'
-import { Bonus } from './components/bonus/bonus'
+import "./App.css";
+import { Hero } from "./components/hero/hero";
+import { Cta } from "./components/cta/cta";
+import { Proof } from "./components/proof/proof";
+import { Benefits } from "./components/Benefits/benefits";
+import { Method } from "./components/method/method";
+import { Bonus } from "./components/bonus/bonus";
+import { Guarantee } from "./components/guarantee/guarantee";
 
 function App() {
-
-
   return (
     <>
-     <Hero/>
-     <Cta />
-     <Proof/>
-     <Benefits/>
-     <Method/>
-     <Bonus/>
+      <Hero />
+      <Cta />
+      <Proof />
+      <Benefits />
+      <Method />
+      <Bonus />
+      <Guarantee />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/guarantee/guarantee.jsx
@@ -0,0 +1,15 @@
+export function Guarantee() {
+  return (
+    <section id="guarantee">
+      <img src="../../../src/assets/svgs/guarantee.svg" />
+      <h3 className="sectionMinTitle greenText">GARANTIA</h3>
+      <h1 className="sectionMText">Risco Zero</h1>
+      <p className="txtP">
+        Vou oferecer uma garantia de 7 dias. Após adquirir o ebook, se você não
+        estiver satisfeito com o conteúdo ou não alcançar os resultados
+        desejados nesse período, basta enviar um email para suporte@oakai.com.
+        Nossa equipe processará o reembolso de 100% do valor investido.
+      </p>
+    </section>
+  );
+}
